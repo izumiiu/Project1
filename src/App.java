53271bbc0bf11ca5dec7e7744393d8b74dfad7bd
izumiiu <peerawat.ez@gmail.com>
2026-02-12 21:34:40 +0700

    testtest2

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Nigga");
+        System.out.println("Hello, Nigga asjdbakjdbaka");
     }
 }
