fa3569cbd62e5ed572a69353817f1d45117bbba6
izumiiu <peerawat.ez@gmail.com>
2026-02-12 22:35:16 +0700

    testtesttest

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello 23974623845394623746293749");
         System.out.println("hello world");
+        System.out.println("hello world");
     }
 }
